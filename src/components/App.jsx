a1a1522ad8159bd9d1dcd5b985128bb86dfc1e61
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-06-07 23:10:01 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,6 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
-//import Home from 'page/Home/Home';
-
-//import Movies from 'page/Movies/Movies';
-//import MovieDetails from 'page/MovieDetails/MovieDetails';
-//import Cast from './Cast/Cast';
-//import Reviews from './Reviews/Reviews';
-//import NoPage from './NoPage/NoPage';
 
 const Home = lazy(() => import('../page/Home/Home'));
 const Movies = lazy(() => import('../page/Movies/Movies'));
@@ -26,7 +19,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<NoPage />} />
+        <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
   );
